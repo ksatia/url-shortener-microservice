b97efa706935bbf3468d4a8d7155452e28ba4eeb
Karan Satia <ksatia@gmail.com>
2020-08-11 10:29:26 -0400

    adds a mongoDB model built from a schema for storing urls

--- models/urlschema.js
+++ models/urlschema.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose")
+const dotenv = require('dotenv')
 const Schema = mongoose.Schema
 
-//create new schema
+// create new schema
+let URLSchema = new Schema({
+  "long_URL":{type: String, required: true},
+  "short_URL":{type: String, required: true}
+})
 
-//create model from the schema
-
-//module.export=^model
+// create model from the schema
+const URL = mongoose.model("URL", URLSchema)
+module.exports = URL
