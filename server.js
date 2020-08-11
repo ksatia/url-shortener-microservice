d03a44d8adc8eafbdd943402c3cf0d2fe4019d82
Karan Satia <ksatia@gmail.com>
2020-08-11 10:30:18 -0400

    removes routing code and adds it to a separate file

--- server.js
+++ server.js
@@ -1,48 +1,19 @@
 'use strict';
-
 const express = require('express');
-const mongo = require('mongodb');
-const mongoose = require('mongoose');
-
+const router = require('./routes/router')
+//const db = mongoose.connection
 const cors = require('cors');
 
+// Routes and DB connection
 var app = express();
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(router)
 
-// Basic Configuration 
+// port config 
 const PORT = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
-
-app.use(cors());
-app.use(express.urlencoded({extended: true}))
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-app.post("/api/shorturl/new", (req, res) => {
-  // let data = ""
-  // req.on('data', (chunk) => {
-  //   data += chunk
-  // })
-  // req.on('end', () => {
-  //   let decodedData = 
-  //   console.log(decodeURIComponent(data))
-  // })
-  console.log(req.body.longURL)
-})
-
+// start server
 app.listen(PORT, function () {
   console.log(`Node.js listening on port ${PORT}...`);
 });
